d55d74aee2a668984664b546d4a78b5c69d6134b
LeahN67 <leah.njuguba@outlook.com>
2020-12-20 21:20:15 +0300

    Updated.

---
+++ Practice1.4.py
@@ -0,0 +1,7 @@
+#File handling
+#CSV files (generally have the .csv extension) hold tabular data that resembles spreadsheet data.
+#Image files (generally with the .png or .jpg extension) hold images for computer vision.
+#Text files (often have the .txt extension) hold unstructured text and are essential for natural language processing.
+#JSON (often have the .json extension) contain semi-structured textual data in a human-readable text-based format.
+#H5 (can have a wide array of extensions) contain semi-structured textual data in a human-readable text-based format. Keras and TensorFlow store neural networks as H5 files.
+#Audio Files (often have an extension such as .au or .wav) contain recorded sound.
